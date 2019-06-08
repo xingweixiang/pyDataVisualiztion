83f647f9ce19bee5819fb17841a3e284d2e4cb3b
Administrator <281349503@qq.com>
2019-06-08 10:08:10 +0800

    读取csv文件

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pyDataVisualiztion
-python数据可视化
+     python数据可视化
+## 运行环境
+     python 3.7.*
+## 目录
+* [python数据可视化](#python数据可视化)
+	* [一、准备工作](#一准备工作)
+		* [1、安装](#1安装)
+		* [2、设置matplotlib参数](#2设置matplotlib参数)
+	* [二、数据处理](#二数据处理)
+		* [1、导入数据](#1导入数据)
+### 一、准备工作
+### 1、安装
+- 安装matplotlib、Numpy、Scipy、图像库(PIL)等。
+### 2、设置matplotlib参数
+- 配置模板以方便各项目共享：有三种方式:当前工作目录，用户级和安装级配置文件
+### 二、数据处理
+### 1、导入数据
+- 从csv文件导入数据：[源码](/example/02_Code/ch02-csvread)
---
+++ example/02_Code/ch02-csvread.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import csv
+import sys
+filename = 'ch02-data.csv'
+data = []
+try:
+    with open(filename) as f:#打开文件
+        reader = csv.reader(f)#读取
+        c = 0
+        for row in reader:
+            if c == 0:
+                header = row#读取头行
+            else:
+                data.append(row)#读取剩余行
+            c += 1
+except csv.Error as e:
+    print ("Error reading CSV file at line %s: %s") % (reader.line_num, e)
+    sys.exit(-1)
+if header:
+    print (header)
+    print ('==================')
+for datarow in data:
+    print (datarow)
